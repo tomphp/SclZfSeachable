0fdb920f2b12fd5ae5698bc11124eb4ae6a97f18
Tom Oram <tom@scl.co.uk>
2013-06-24 20:03:01 +0100

    Implement fetchAll

--- src/SclZfSearchable/Mapper/SearchableDoctrineMapper.php
+++ src/SclZfSearchable/Mapper/SearchableDoctrineMapper.php
@@ -2,6 +2,7 @@
 
 namespace SclZfSearchable\Mapper;
 
+use Doctrine\ORM\QueryBuilder;
 use SclZfSearchable\SearchableRepositoryInterface;
 use SclZfSearchable\SearchInfo\SearchInfoInterface;
 use SclZfUtilities\Mapper\GenericDoctrineMapper;
@@ -14,6 +15,25 @@ use SclZfUtilities\Mapper\GenericDoctrineMapper;
 class SearchableDoctrineMapper extends GenericDoctrineMapper implements
     SearchableRepositoryInterface
 {
+    /**
+     * The name to use for the entity in the query.
+     */
+    const ENTITY = 'e';
+
+    /**
+     * The info to used in the search.
+     *
+     * @var SearchInfoInterface
+     */
+    protected $searchInfo;
+
+    /**
+     * A list of fields to be used in the search.
+     *
+     * @var string[]
+     */
+    protected $searchFields = array();
+
     /**
      * {@inheritDoc}
      *
@@ -28,12 +48,83 @@ class SearchableDoctrineMapper extends GenericDoctrineMapper implements
     }
 
     /**
+     * Set the list of fields to use in a search.
+     *
+     * @param  string[] $fields
+     * @return self
+     */
+    public function setSearchFields(array $fields)
+    {
+        $this->searchFields = $fields;
+
+        return $this;
+    }
+
+    /**
+     * Applies the given search terms to the query builder's where clause.
+     *
+     * @param  QueryBuilder $qb
+     * @return QueryBuilder
+     */
+    public function queryAddSearch(QueryBuilder $qb)
+    {
+        if (null === $this->searchInfo->getSearch()) {
+            return $qb;
+        }
+
+        $expr = $qb->expr()->orX();
+
+        foreach ($this->searchFields as $field) {
+            $expr->add($qb->expr()->like(self::ENTITY . '.' . $field, ':search'));
+        }
+
+        $qb->where($expr);
+
+        $qb->setParameter('search', '%' . $this->searchInfo->getSearch() . '%');
+
+        return $qb;
+    }
+
+    /**
+     * Sets up the query's order by clause.
+     *
+     * @param  QueryBuilder $qb
+     * @return QueryBuilder
+     */
+    public function queryAddOrderBy(QueryBuilder $qb)
+    {
+        if (null === $this->searchInfo->getOrderBy()) {
+            return $qb;
+        }
+
+        $field = self::ENTITY . '.' . $this->searchInfo->getOrderBy();
+
+        $order = (SearchInfoInterface::SORT_ASC === $this->searchInfo->getOrder()) ? 'ASC' : 'DESC';
+
+        $qb->orderBy($field, $order);
+
+        return $qb;
+    }
+
+
+
+    /**
      * Returns all entities that match the search criteria.
      *
      * @return array
-     * @todo   Write this method
      */
     public function fetchAll()
     {
+        $qb = $this->entityManager->createQueryBuilder();
+
+        $qb->select(self::ENTITY)
+           ->from($this->entityName, self::ENTITY);
+
+        $this->queryAddSearch($qb);
+
+        $this->queryAddOrderBy($qb);
+
+        // @todo Maybe convert this to an array?
+        return $qb->getQuery->getResult();
     }
 }
